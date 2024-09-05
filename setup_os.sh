7002b3c9a61bcd62ff328c8790e8380ac41c0958
Ronald Eytchison <ronald.eytchison@trailofbits.com>
2024-09-05 23:10:35 +0000

    Configure cluster IP

--- setup_os.sh
+++ setup_os.sh
@@ -3,9 +3,16 @@
 # It configures OpenSearch and runs a test workload to validate the installation.
 # Usage: bash setup_os.sh
 # Tested on Ubuntu 22.04
+# Environment variables:
+#   OS_PASSWORD: password for opensearch
+#   CLUSTER_IP: IP address for cluster. Recommend the private IP from `hostname -I`.
 set -ex
 if [ -z "${OS_PASSWORD}" ]; then
     echo "OS_PASSWORD environment variable is empty or unset"
+    exit 1
+fi
+if [ -z "${CLUSTER_IP}" ]; then
+    echo "CLUSTER_IP environment variable is empty or unset"
     exit 1
 fi
 
@@ -39,6 +46,20 @@ sudo apt-get update
 sudo apt list -a opensearch
 sudo env OPENSEARCH_INITIAL_ADMIN_PASSWORD="$OS_PASSWORD" apt-get -y install opensearch=2.16.0
 
+# add user to opensearch group so they can read the config files
+sudo adduser $(whoami) opensearch
+# Modify configuration files https://opensearch.org/docs/latest/install-and-configure/install-opensearch/debian/#step-3-set-up-opensearch-in-your-environment
+echo "Modifying: /etc/opensearch/opensearch.yml"
+echo "### Begin user configuration ###" | sudo tee -a /etc/opensearch/opensearch.yml
+echo "discovery.type: single-node" | sudo tee -a /etc/opensearch/opensearch.yml
+echo "plugins.security.disabled: false" | sudo tee -a /etc/opensearch/opensearch.yml
+sudo sed -i -e "/network.host:/c\network.host: $CLUSTER_IP # user modified" /etc/opensearch/opensearch.yml
+echo "### End user configuration ###" | sudo tee -a /etc/opensearch/opensearch.yml
+
+echo "Modifying: /etc/opensearch/jvm.options"
+sudo sed -i -e "s/^-Xms[0-9a-z]*$/$JVM_MIN_HEAP/g" /etc/opensearch/jvm.options
+sudo sed -i -e "s/^-Xmx[0-9a-z]*$/$JVM_MAX_HEAP/g" /etc/opensearch/jvm.options
+
 sudo systemctl daemon-reload
 sudo systemctl enable opensearch.service
 sudo systemctl start opensearch.service
@@ -46,24 +67,11 @@ sudo systemctl status --no-pager opensearch.service
 
 apt show opensearch
 
-curl -X GET https://localhost:9200 -u "admin:$OS_PASSWORD" --insecure
-curl -X GET https://localhost:9200/_cat/plugins?v -u "admin:$OS_PASSWORD" --insecure
-
-# Modify configuration files https://opensearch.org/docs/latest/install-and-configure/install-opensearch/debian/#step-3-set-up-opensearch-in-your-environment
-echo "Modifying: /etc/opensearch/opensearch.yml"
-echo "### Begin user configuration ###" | sudo tee -a /etc/opensearch/opensearch.yml
-echo "discovery.type: single-node" | sudo tee -a /etc/opensearch/opensearch.yml
-echo "plugins.security.disabled: false" | sudo tee -a /etc/opensearch/opensearch.yml
-echo "### End user configuration ###" | sudo tee -a /etc/opensearch/opensearch.yml
-
-echo "Modifying: /etc/opensearch/jvm.options"
-sudo sed -i -e "s/^-Xms[0-9a-z]*$/$JVM_MIN_HEAP/g" /etc/opensearch/jvm.options
-sudo sed -i -e "s/^-Xmx[0-9a-z]*$/$JVM_MAX_HEAP/g" /etc/opensearch/jvm.options
-
-sudo systemctl restart opensearch
+curl -X GET https://$CLUSTER_IP:9200 -u "admin:$OS_PASSWORD" --insecure
+curl -X GET https://$CLUSTER_IP:9200/_cat/plugins?v -u "admin:$OS_PASSWORD" --insecure
 
 # Test OSB (check for errors)
 opensearch-benchmark execute-test --distribution-version=2.16.0 --workload=big5 --workload-params corpus_size:100,number_of_replicas:0,target_throughput:"" --test-mode
 
 # stop opensearch service
-sudo systemctl stop opensearch
+sudo systemctl restart opensearch
