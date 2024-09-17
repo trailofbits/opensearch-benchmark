0734b95af319d6e4366c2f49cc2a7c9d28457735
Henrik Brodin <90325907+hbrodin@users.noreply.github.com>
2024-09-17 15:48:53 +0200

    Remove debug/commented code

--- infra/modules/opensearch/os_cluster.sh
+++ infra/modules/opensearch/os_cluster.sh
@@ -16,49 +16,35 @@ cd /mnt || exit 1
 mkdir -p $INSTALL_PATH
 wget $DOWNLOAD_URL
 tar -xvf $INSTALL_FILENAME -C $INSTALL_ROOT
-ls -la $INSTALL_PATH > a.tmp
 rm $INSTALL_FILENAME
 
 # Update the configuration to allow incoming connections
 sed -i "s/#network.host: .*/network.host: 0.0.0.0/" $CONFIG_FILE 
 echo "discovery.type: single-node" >> $CONFIG_FILE
 
-touch b
 # JDK location
 export OPENSEARCH_JAVA_HOME=$INSTALL_PATH/jdk
 echo "export OPENSEARCH_JAVA_HOME=$$OPENSEARCH_JAVA_HOME" >> ~/.bashrc
 
-touch c
 # Fix the JVM size
 GB=$(echo "$(cat /proc/meminfo | grep MemTotal | awk '{print $2}') / (1024*1024*2)" | bc)
 sed -i "s/-Xms1g/-Xms${GB}g/" $JVM_CONFIG
 sed -i "s/-Xmx1g/-Xmx${GB}g/" $JVM_CONFIG
-touch d
 
-touch e
 # Run opensearch startup script with security demo configuration
 OPENSEARCH_INITIAL_ADMIN_PASSWORD=$OS_PASSWORD $INSTALL_PATH/opensearch-tar-install.sh &> opensearch.log &
 SERVER_PID=$!
 
-touch f
 echo "Waiting for server to boot"
 
 echo "Waiting for server to boot"
 # Wait for OpenSearch to start
 while ! curl --max-time 5 -ks https://localhost:9200 2>&1 >/dev/null ; do
-    touch g
     echo "Waiting for OpenSearch to start"
     sleep 1
 done 
-touch h
 
 echo "OpenSearch responds on port 9200, now verify credentials"
 curl -X GET https://localhost:9200 -u "admin:$OS_PASSWORD" --insecure || (echo "Failed to query server" && false)
 echo
 echo "Server up and running (pid $SERVER_PID)"
-# kill $SERVER_PID
-
-# cd /mnt || exit 1
-
-
-#sudo apt update && sudo -E DEBIAN_FRONTEND=noninteractive apt install -y socat
