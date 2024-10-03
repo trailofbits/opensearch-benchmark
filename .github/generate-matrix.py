a7e5f98b7409afea95460b2ea3f599eb7691c8ca
Riccardo Schirone <riccardo.schirone@trailofbits.com>
2024-10-03 16:38:32 +0200

    fix generate matrix

--- .github/generate-matrix.py
+++ .github/generate-matrix.py
@@ -5,21 +5,30 @@ workloads = sys.argv[1].split(',')
 workload_params = sys.argv[2]
 cluster_types = sys.argv[3].split(',')
 
-output = {
-  "cluster_type": cluster_types,
-  "workload": workloads,
-  "include": [
-    {
-      "cluster_type": "OpenSearch",
-      "s3_bucket_name": "os-snapshots-osb"
-    },
-    {
-      "cluster_type": "ElasticSearch",
-      "s3_bucket_name": "es-snapshots-osb"
-    },
+includes = [
     {
       "workload_params": workload_params
     }
-  ]
+]
+
+if "OpenSearch" in cluster_types:
+    includes = [
+        {
+            "cluster_type": "OpenSearch",
+            "s3_bucket_name": "os-snapshots-osb"
+        }
+    ] + includes
+if "ElasticSearch" in cluster_types:
+    includes = [
+        {
+            "cluster_type": "ElasticSearch",
+            "s3_bucket_name": "es-snapshots-osb"
+        }
+    ] + includes
+
+output = {
+  "cluster_type": cluster_types,
+  "workload": workloads,
+  "include": includes,
 }
 print(json.dumps(output))
