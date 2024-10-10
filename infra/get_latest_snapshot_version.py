10a6157159d8998c929860413b85e4c1adca9502
Riccardo Schirone <riccardo.schirone@trailofbits.com>
2024-10-10 11:20:16 +0200

    make get_latest_snapshot_version script more robust

--- infra/get_latest_snapshot_version.py
+++ infra/get_latest_snapshot_version.py
@@ -4,46 +4,68 @@ import json
 from datetime import datetime
 import re
 
+
+def new_snapshot_version() -> str:
+    return datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+
+def is_version_format(version: str) -> bool:
+    return re.match(r"\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}", version)
+
+
 input_map = json.loads(input())
 
-if input_map['snapshot_version'] == 'latest':
-    s3_prefix = f"{input_map['cluster_type']}/{input_map['cluster_version']}/{input_map['workload']}/"
+s3_prefix = f"{input_map['cluster_type']}/{input_map['cluster_version']}/{input_map['workload']}"
+if input_map["snapshot_version"] == "latest":
     cmd = [
         "aws",
-        "s3api",
-        "list-objects-v2",
-        "--bucket",
-        input_map["s3_bucket_name"],
-        "--prefix",
-        s3_prefix,
-        "--query",
-        "sort_by(Contents, &LastModified)[-1].Key",
-        "--output",
-        "text"
+        "s3",
+        "ls",
+        f"s3://{input_map['s3_bucket_name']}/{s3_prefix}/",
     ]
     try:
         res = subprocess.check_output(cmd, universal_newlines=True).strip()
-        latest_version = res.split('/')[3] if len(res.split('/')) > 3 else None
-    except subprocess.CalledProcessError:
-        latest_version = None
-        print(f"Error while calling aws s3api list-objects-v2", file=sys.stderr)
-elif input_map['snapshot_version'] == 'new':
-    latest_version = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        subdirs = [
+            x.split()[1] for x in res.splitlines() if x.strip() and len(x.split()) == 2
+        ]
+        subdirs = [x[:-1] for x in subdirs if x.endswith("/")]
+        versions = [x for x in subdirs if is_version_format(x)]
+        sorted_versions = (
+            x
+            for x in sorted(
+                versions,
+                key=lambda x: datetime.strptime(x, "%Y-%m-%d_%H-%M-%S"),
+                reverse=True,
+            )
+        )
+        latest_version = next(sorted_versions, None)
+    except subprocess.CalledProcessError as e:
+        print(f"Error while calling aws s3 ls: {e}", file=sys.stderr)
+        sys.exit(1)
+elif input_map["snapshot_version"] == "new":
+    latest_version = new_snapshot_version()
 else:
-    latest_version = input_map['snapshot_version']
+    latest_version = input_map["snapshot_version"]
+    cmd = [
+        "aws",
+        "s3",
+        "ls",
+        f"s3://{input_map['s3_bucket_name']}/{s3_prefix}/{latest_version}",
+    ]
+    try:
+        res = subprocess.check_output(cmd, universal_newlines=True).strip()
+        subdirs = [
+            x.split()[1] for x in res.splitlines() if x.strip() and len(x.split()) == 2
+        ]
+        subdirs = [x[:-1] for x in subdirs if x.endswith("/")]
+        if len(subdirs) != 1:
+            print(f"Snapshot version {latest_version} not found", file=sys.stderr)
+            sys.exit(1)
 
-if latest_version is None:
-    # Default to current date
-    latest_version = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    print(f"Could not find a latest version, defaulting to {latest_version}", file=sys.stderr)
+    except subprocess.CalledProcessError as e:
+        print(f"Error while calling aws s3 ls: {e}", file=sys.stderr)
+        sys.exit(1)
 
-# Ensure that $LATEST_VERSION is in a date format '%Y-%m-%d_%H-%M-%S'
-if not re.match(r'\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}', latest_version):
-    print(f"Could not find a valid latest version (correct date format), error", file=sys.stderr)
-    sys.exit(1)
-
-output = {
-    "latest_version": latest_version
-}
+output = {"latest_version": latest_version}
 print(f"Latest version: {latest_version}", file=sys.stderr)
 print(json.dumps(output))
