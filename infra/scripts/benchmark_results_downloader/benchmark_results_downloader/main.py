118b9bf7dc7ff7a60b80b0bb6c5ffda13b475b4e
Stefano Bonicatti <stefano.bonicatti@gmail.com>
2024-10-11 22:49:03 +0200

    benchmark_results_downloader: Initial commit

---
+++ infra/scripts/benchmark_results_downloader/benchmark_results_downloader/main.py
@@ -0,0 +1,398 @@
+import argparse
+import csv
+import itertools
+import json
+import logging
+import os
+from collections.abc import Collection, Mapping
+from datetime import datetime
+from operator import attrgetter
+from pathlib import Path
+from typing import Any
+
+from opensearchpy import OpenSearch
+from opensearchpy.transport import Transport
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class BenchmarkResult:
+    def __init__(
+        self,
+        run_group: datetime,
+        engine: str,
+        engine_version: str,
+        run: str,
+        workload: str,
+        test_procedure: str,
+        workload_params: dict[str, str],
+        shard_count: int,
+        replica_count: int,
+        operation: str,
+        metric_name: str,
+        p50: str,
+        p90: str,
+    ):
+        self.RunGroup: datetime = run_group
+        self.Engine = engine
+        self.EngineVersion = engine_version
+        self.Run = run
+        self.Workload = workload
+        self.TestProcedure = test_procedure
+        self.WorkloadParams = workload_params
+        self.ShardCount = shard_count
+        self.ReplicaCount = replica_count
+        self.Operation = operation
+        self.MetricName = metric_name
+        self.P50 = p50
+        self.P90 = p90
+
+
+class VerboseTransport(Transport):
+    def perform_request(
+        self,
+        method: str,
+        url: str,
+        params: Mapping[str, Any] | None = None,
+        body: Any = None,
+        timeout: int | float | None = None,
+        ignore: Collection = (),
+        headers: Mapping[str, str] | None = None,
+    ) -> Any:
+        # Log the request details
+        logger.info(f"Request Method: {method}")
+        logger.info(f"Request URL: {url}")
+        logger.info(f"Request Headers: {headers}")
+        logger.info(f"Request Params: {params}")
+        if body:
+            logger.info(f"Request Body: {json.dumps(body, indent=2)}")
+
+        logger.info(f"Hosts: {self.hosts}")
+
+        # Perform the actual request
+        return super().perform_request(method, url, params, body, timeout, ignore, headers)
+
+
+def main() -> int:
+    args_parser = argparse.ArgumentParser(
+        description="Downloads benchmark results from an OpenSearch datastore as CSVs "
+        "with the format <run-group>-<engine>-<engine version>-<workload>-<test-procedure>.csv "
+        "into a provided folder"
+    )
+
+    args_parser.add_argument(
+        "--host",
+        help="Hostname of the datastore to download the benchmark results from",
+        required=True,
+        type=str,
+    )
+    args_parser.add_argument(
+        "--port",
+        help="Port of the datastore to download the benchmark results from (default: %(default)s)",
+        type=int,
+        default=443,
+    )
+    args_parser.add_argument(
+        "--benchmark-data",
+        help="Path to an existing folder to download the benchmark data to",
+        type=Path,
+        required=True,
+    )
+    args_parser.add_argument(
+        "--from",
+        help="Download results starting from this date (inclusive). Format is YYYY-MM-DD",
+        dest="from_arg",
+        type=str,
+        required=True,
+    )
+    args_parser.add_argument(
+        "--to",
+        help="Download results up to this date (inclusive). Format is YYYY-MM-DD. Default is now",
+        dest="to_arg",
+        type=str,
+        default=None,
+    )
+    args_parser.add_argument(
+        "--run-type",
+        help="Which benchmark data run type (normally dev or official) to download (default: %(default)s)",
+        type=str,
+        default="official",
+    )
+    args_parser.add_argument(
+        "--debug-request",
+        help="Logs in stdout the query request (default: %(default)s)",
+        action="store_true",
+        default=False,
+    )
+    args_parser.add_argument(
+        "--debug-response",
+        help="Logs in stdout the JSON response (default: %(default)s)",
+        action="store_true",
+        default=False,
+    )
+
+    args = args_parser.parse_args()
+
+    password = os.environ.get("DS_PASSWORD")
+
+    if password is None:
+        print("Datastore password missing, please pass it as the DS_PASSWORD environment variable")
+        return 1
+
+    benchmark_data_folder: Path = args.benchmark_data
+
+    if not benchmark_data_folder.exists():
+        print(f"Could not find the provided benchmark data folder at {benchmark_data_folder}")
+        return 1
+
+    try:
+        start_date = args.from_arg
+        datetime.strptime(start_date, "%Y-%m-%d")
+    except ValueError:
+        print("Wrong format for the 'from' parameter, please use a date in YYYY-MM-DD format")
+        return 1
+
+    if args.to_arg is None:
+        end_date = "now"
+    else:
+        try:
+            end_date = args.to_arg
+            datetime.strptime(end_date, "%Y-%m-%d")
+        except ValueError:
+            print("Wrong format for the 'to' parameter, please use a date in YYYY-MM-DD format")
+            return 1
+
+    if start_date > end_date:
+        print("Wrong date range. The date in --from needs to be the same or before the one in --to")
+        return 1
+
+    query = {
+        "size": 10000,
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "range": {
+                            "test-execution-timestamp": {
+                                "gte": start_date,
+                                "lte": end_date,
+                                "format": "strict_year_month_day",
+                            }
+                        }
+                    },
+                    {"terms": {"user-tags.run-type": [args.run_type]}},
+                    {"exists": {"field": "operation"}},
+                    {"exists": {"field": "value.50_0"}},
+                    {"exists": {"field": "value.90_0"}},
+                    {"exists": {"field": "user-tags.run-group"}},
+                    {"exists": {"field": "user-tags.run"}},
+                    {"exists": {"field": "user-tags.engine-type"}},
+                    {"exists": {"field": "user-tags.shard-count"}},
+                    {"exists": {"field": "user-tags.replica-count"}},
+                ],
+            },
+        },
+    }
+
+    # Use VerboseTransport to print more information on the request being done
+    transport_class = VerboseTransport if args.debug_request else Transport
+
+    client = OpenSearch(
+        hosts=[{"host": args.host, "port": args.port}],
+        http_compress=True,
+        http_auth=("admin", password),
+        use_ssl=True,
+        verify_certs=False,
+        ssl_assert_hostname=False,
+        ssl_show_warn=False,
+        transport_class=transport_class,
+    )
+
+    response = client.search(body=query, index="benchmark-results*")
+
+    if response is None:
+        print(f"Failed to get results: { response }")
+        return 1
+
+    hits = response.get("hits")
+
+    if hits is None:
+        print("Failed to find the top hits key")
+        return 1
+
+    documents = hits.get("hits")
+
+    if documents is None:
+        print("Failed to get any documents")
+        return 1
+
+    if args.debug_response:
+        print(f"Documents: {len(documents)}")
+
+    results: list = []
+
+    if args.debug_response:
+        print(json.dumps(response))
+
+    if args.benchmark_data is not None:
+        all_workload_params_names_set = set()
+        all_workload_params_names = []
+        workload_params_name_to_pos = {}
+
+        for document in documents:
+            source = document["_source"]
+            engine_version = source["distribution-version"]
+            workload = source["workload"]
+            operation = source["operation"]
+            metric_name = source["name"]
+            metric_value = source["value"]
+            test_procedure = source["test_procedure"]
+            workload_params_dict = source["workload-params"]
+            p50 = metric_value["50_0"]
+            p90 = metric_value["90_0"]
+            user_tags = source["user-tags"]
+            run = user_tags["run"]
+            engine = user_tags["engine-type"]
+            run_group_str = user_tags["run-group"]
+            shard_count = user_tags["shard-count"]
+            replica_count = user_tags["replica-count"]
+
+            # Parse into a proper date for sorting purposes
+            run_group_date = datetime.strptime(run_group_str, "%Y_%m_%d_%H_%M_%S")
+
+            for workload_param in workload_params_dict:
+                all_workload_params_names_set.add(f"workload\\.{workload_param}")
+
+            results.append(
+                BenchmarkResult(
+                    run_group_date,
+                    engine,
+                    engine_version,
+                    run,
+                    workload,
+                    test_procedure,
+                    workload_params_dict,
+                    shard_count,
+                    replica_count,
+                    operation,
+                    metric_name,
+                    p50,
+                    p90,
+                )
+            )
+
+        all_workload_params_names = list(all_workload_params_names_set)
+
+        for index in range(len(all_workload_params_names)):
+            workload_params_name_to_pos[all_workload_params_names[index]] = index
+
+        fields_sort_priority = [
+            "RunGroup",
+            "Engine",
+            "EngineVersion",
+            "Workload",
+            "TestProcedure",
+            "WorkloadParams",
+            "Run",
+            "MetricName",
+        ]
+        sorted_benchmark_results: list[BenchmarkResult] = sorted(
+            results, key=attrgetter(*fields_sort_priority)
+        )
+
+        # These pre and post headers exists to support a dynamic number of columns
+        # of workload params which will be placed in between
+        headers_pre = [
+            "user-tags\\.run-group",
+            "user-tags\\.engine-type",
+            "distribution-version",
+            "workload",
+            "test-procedure",
+        ]
+
+        headers_post = [
+            "user-tags\\.shard-count",
+            "user-tags\\.replica-count",
+            "user-tags\\.run",
+            "operation",
+            "name",
+            "value\\.50_0",
+            "value\\.90_0",
+        ]
+
+        current_run_group = None
+        current_engine = None
+        current_engine_version = None
+        current_workload = None
+        current_test_procedure = None
+        csv_file = None
+        csv_writer = None
+
+        all_workload_params_len = len(all_workload_params_names)
+        for result in sorted_benchmark_results:
+            new_run_group = result.RunGroup
+            new_engine = result.Engine
+            new_engine_version = result.EngineVersion
+            new_workload = result.Workload
+            new_test_procedure = result.TestProcedure
+
+            # Every time any of these columns changes values, we open a new file.
+            # Given how we have previously sorted
+            if (
+                current_run_group != new_run_group
+                or current_engine != new_engine
+                or current_engine_version != new_engine_version
+                or current_workload != new_workload
+                or current_test_procedure != current_test_procedure
+            ):
+                current_run_group = new_run_group
+                current_engine = new_engine
+                current_engine_version = new_engine_version
+                current_workload = new_workload
+                current_test_procedure = new_test_procedure
+
+                if csv_file is not None:
+                    csv_file.close()
+                csv_file_path = (
+                    benchmark_data_folder / f"{int(current_run_group.timestamp())}-{current_engine}"
+                    f"-{current_engine_version}-{current_workload}-{current_test_procedure}.csv"
+                )
+                csv_file = csv_file_path.open("w", newline="")
+                csv_writer = csv.writer(csv_file, delimiter=",", quotechar='"')
+                headers = itertools.chain(headers_pre, all_workload_params_names, headers_post)
+                csv_writer.writerow(headers)
+
+            values_pre = [
+                result.RunGroup,
+                result.Engine,
+                result.EngineVersion,
+                result.Workload,
+                result.TestProcedure,
+            ]
+            values_post = [
+                result.ShardCount,
+                result.ReplicaCount,
+                result.Run,
+                result.Operation,
+                result.MetricName,
+                result.P50,
+                result.P90,
+            ]
+
+            # Ensure the size is equal to the list of all workload params we encountered
+            workload_params_values = [""] * all_workload_params_len
+
+            # Then match the workload param name to the correct column position
+            for key, value in result.WorkloadParams.items():
+                workload_params_values[workload_params_name_to_pos[f"workload\\.{key}"]] = value
+
+            values = itertools.chain(values_pre, workload_params_values, values_post)
+            csv_writer.writerow(values)
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
