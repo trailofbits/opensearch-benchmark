04e977a7df6c19a6f797a99507a8dece9bf0c9d4
Ronald Eytchison <ronald.eytchison@trailofbits.com>
2024-11-04 21:33:36 -0500

    Move vectorsearch workload params to tfvars and support multiline workload params

--- infra/scripts/benchmark.sh
+++ infra/scripts/benchmark.sh
@@ -29,7 +29,12 @@ INDEX_NAME=$(workload_index_name $WORKLOAD)
 
 # This comes from the user `terraform.tfvars` configuration file
 # shellcheck disable=SC2154
-WORKLOAD_PARAMS="$${WORKLOAD_PARAMS:-${workload_params}}"
+if [ -z "$WORKLOAD_PARAMS" ]; then
+    WORKLOAD_PARAMS=$(cat <<EOF
+${workload_params}
+EOF
+)
+fi
 
 # This comes from the user `terraform.tfvars` configuration file
 # shellcheck disable=SC2154
