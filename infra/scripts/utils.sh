baa26647e427768f65df8222bfcbf1a73b318f04
Ronald Eytchison <58823072+reytchison@users.noreply.github.com>
2024-11-27 08:49:01 -0500

    Switch to 10m cohere corpus (#201)
    * Switch to 10m cohere corpus
    
    * Increase ingest job timeout
    
    * Update docs and shards count for vectorsearch
    
    * Update expected shard count
    
    * Increase ingest timeout
    
    * Allow 5,000 duplicates in vectorsearch ingest
    
    * Increase max parallel jobs to 12
    
    * Fix shellcheck error

--- infra/scripts/utils.sh
+++ infra/scripts/utils.sh
@@ -3,18 +3,19 @@
 get_doc_count() {
     local workload=$1
 
+    # single expected doc count, or a min and max doc count
     declare -A doc_counts
-    doc_counts["big5"]=116000000
-    doc_counts["nyc_taxis"]=165346691 # NOTE: should be 165346692 but there's an issue with one document
-    doc_counts["pmc"]=574199
-    doc_counts["noaa"]=33659481
-    doc_counts["vectorsearch"]=1000000
-    doc_counts["noaa_semantic_search"]=33659481
+    doc_counts["big5"]="116000000"
+    doc_counts["nyc_taxis"]="165346691" # NOTE: should be 165346692 but there's an issue with one document
+    doc_counts["pmc"]="574199"
+    doc_counts["noaa"]="33659481"
+    doc_counts["vectorsearch"]="10000000 10005000" # min and max doc count to allow 5000 duplicates
+    doc_counts["noaa_semantic_search"]="33659481"
 
     # Check if the workload exists in the associative array
     if [[ -v "doc_counts[$workload]" ]]; then
         # Get the DOC_COUNT based on the WORKLOAD
-        echo "${doc_counts[$workload]}"
+        echo ${doc_counts[$workload]}
     else
         # Default value
         echo "1000"
@@ -27,7 +28,7 @@ get_shards_count() {
     declare -A shards_count
     shards_count["big5"]=1
     shards_count["pmc"]=5
-    shards_count["vectorsearch"]=3
+    shards_count["vectorsearch"]=6
     shards_count["noaa_semantic_search"]=6
 
     # Check if the workload exists in the associative array
@@ -51,6 +52,35 @@ check_value () {
     fi
 }
 
+check_value_range () {
+    local name=$1
+    local expected_min=$2
+    local expected_max=$3
+    local actual=$4
+
+    if (( $actual < $expected_min )); then
+        printf 'Incorrect %s value, actual %s is less than expected min %s\n' "$name" "$actual" "$expected_min"
+        exit 1
+    fi
+
+    if (( $actual > $expected_max )); then
+        printf 'Incorrect %s value, actual %s is greater than expected max %s\n' "$name" "$actual" "$expected_max"
+        exit 1
+    fi
+}
+
+check_doc_count () {
+    local doc_count=$1
+    local workload=$2
+    read -r min_doc_count max_doc_count <<< "$(get_doc_count $workload)"
+    if [[ -z "$max_doc_count" ]]; then
+        expected_doc_count="$min_doc_count"
+        check_value "document count" "${DOC_COUNT:-$expected_doc_count}" "$doc_count"
+    else
+        check_value_range "document count" "${DOC_COUNT:-$min_doc_count}" "${DOC_COUNT:-$max_doc_count}" "$doc_count"
+    fi
+}
+
 check_params () {
     local user=$1
     local password=$2
@@ -66,8 +96,7 @@ check_params () {
             --request GET "$host/$index/_count" \
         | jq --raw-output '"\(.count) \(._shards.total) \(._shards.failed) \(._shards.skipped)"' \
     )
-
-    check_value "document count" "${DOC_COUNT:-$(get_doc_count $workload)}" "$doc_count"
+    check_doc_count "$doc_count" "$workload"
     check_value "total shards count" "${TOTAL_SHARDS:-$(get_shards_count $workload)}" "$shards_total"
     check_value "failed shards count" "${FAILED_SHARDS:-0}" "$shards_failed"
     check_value "skipped shards count" "${SKIPPED_SHARDS:-0}" "$shards_skipped"
